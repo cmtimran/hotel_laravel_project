434256d26dfa59a0e9b24a8f3cb4acc4889a551c
aya797 <ayasakr797@gmail.com>
2020-10-26 14:56:04 +0200

    'seeder'

---
+++ database/seeds/RoomsTablesSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+
+use App\Room;
+use Illuminate\Database\Seeder;
+use Illuminate\Eloquent\model;
+use Illuminate\Support\Str;
+use Faker\Factory;
+
+
+class RoomsTablesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for($i=0;$i<100;$i++){
+             Room::create([
+            'floar' => int::random(10),
+            'view' => Str::random(20),
+            'type' => Str::random(20),
+            'status' => rand(0, 1),           
+        ]);}
+    }
+}
