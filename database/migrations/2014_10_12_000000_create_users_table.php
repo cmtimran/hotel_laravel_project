8222338c7659f0c0071baa255e3bb05c4d2d2aa5
aya797 <ayasakr797@gmail.com>
2020-10-26 23:15:26 +0200

    'add'

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('email_verified_at')->nullable();  // created at ,updated at
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
