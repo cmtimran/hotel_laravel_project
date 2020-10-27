25ab62e2e00fcf4a07e4bc26816920614ad6e4bb
aya797 <ayasakr797@gmail.com>
2020-10-28 00:51:07 +0200

    'add'

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -42,13 +42,13 @@
 
 
 
-        <footer class="main-footer">
+        {{-- <footer class="main-footer">
               <div class="pull-right hidden-xs">
                <b>Version</b> 2.4.0
                </div>
               <strong>Copyright &copy; 2020<a href="">AAH</a>.</strong> All rights
                   reserved.
-        </footer> 
+        </footer>  --}}
     </div>
 </body>
        
